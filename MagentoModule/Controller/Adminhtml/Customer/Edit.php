64a1f8cffd7ae3363edd03dc431d40fd1db74dab
developerRitesh <riteshprajapati3.14@gmail.com>
2018-06-02 12:47:55 +0500

    created a basic magento form with connectivity or data

---
+++ MagentoModule/Controller/Adminhtml/Customer/Edit.php
@@ -0,0 +1,24 @@
+<?php
+namespace MagentoTwo\MagentoModule\Controller\Adminhtml\Customer;
+ 
+class Edit extends \Magento\Backend\App\Action
+{
+    protected $resultPageFactory = false;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    )
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend((__('Edit Customer')));
+
+        return $resultPage;
+    }
+}
